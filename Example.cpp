de02e1f3aa6d245c73d467b143b87e2525bc3ba2
Nick <91141907+seoulxss@users.noreply.github.com>
2023-06-05 17:53:59 +0200

    Update Example.cpp
    Added more descr. and changed sth

--- Example.cpp
+++ Example.cpp
@@ -10,11 +10,13 @@ int main()
 	//											//
 
 
-
 	//How to create the object
 	//Gets init with console only & Successful level
+	//Both are the same!
 	Cseoul_logger L1(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
-	//Printing with "Suuccess" before the text and then also printing "Success!"
+	Cseoul_logger L1_1(Cseoul_logger::LOG_MODE::LOG_CONSOLE, Cseoul_logger::LOG_LEVEL::LOG_SUCCESSFUL);
+
+	//Printing with "Success" before the text and then also printing "Success!"
 	L1.Print("Success!");
 
 	//Set to false to hide the cur time
@@ -33,17 +35,19 @@ int main()
 	//If u have a dll to inject somewhere, it will be created in the applications exe folder
 	L1.Create_Log_File("Test_log_1", true, "", "Cseoul_logger.exe");
 
-	//This will create a Text file in a specific lcoation
+	//This will create a Text file in a specific location
 	//As you can see, you can leave the "name_of_application" var empty, it will not affect anything
 	//*WARNING* Don't ever put 0 or NULL or nullptr in these empty vars, it will crash!
+	//Create only one file per logger!
 	L1.Create_Log_File("Test_log_2", false, "C:\\Users\\teilo\\Desktop\\Test!", "");
+
+	//Here's the other way:
+	/* L1.Create_Log_File("Test_log_2", true, "", "Soul_logger.exe"); */
 
 
 	//Dont forget to set the log_mode after creating the file!
 	//U still have to create a file if u init the Logger with LOG_FILE_AND_CONSOLE
 	L1.set_log_mode(Cseoul_logger::LOG_FILE_AND_CONSOLE);
-
-
 
 
 	//U can use this to shutdown the logger (Destroying)
@@ -52,8 +56,6 @@ int main()
 	//*WARNING*: Only Shutdown at the end of your program!
 	//**************** READ LINE 120 ********************
 	L1.Shutdown_Logger();
-
-
 
 
 	//														//
@@ -90,7 +92,7 @@ int main()
 	//you can do that also with dec numbers:
 	L2.Print("Addr-dec: " + Logger::helper::value_to_string_dec(0x5555));
 
-	std::cout << CCounter<Cseoul_logger>::Oustanding_Objects() << "STF" << Logger::LOG_MACRO__::ENDL;
+	std::cout << CCounter<Cseoul_logger>::Oustanding_Objects() << "STF" << priv_logger::LOG_MACRO__::ENDL;
 
 
 	//														//
@@ -101,7 +103,7 @@ int main()
 
 
 	//If u want to use multiple logger, init them all at ones!
-		//					Example					//
+	//					Example					//
 
 	Cseoul_logger First_Logger(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
 	Cseoul_logger Second_Logger(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
@@ -124,7 +126,7 @@ int main()
 	//Everything that gets printed here will be ignored (Even crash)
 	//For example:
 	//std::cout << "This will not be printed! AND Maybe even crash!" << "\n";
-
+	//return 0
 
 
 	//														//
@@ -136,22 +138,22 @@ int main()
 
 	//Creating a new logger and a array
 	Cseoul_logger array_logger(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
-	auto array1 = std::array<int, 10> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	auto array1 = std::array<int, 10>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 	//Printing the array at pos 0
 	array_logger.Print("array1: " + std::to_string(array1.at(0)));
 	array_logger.Print("array1: " + std::to_string(array1[0]));
 
-	//Iterating over a array
+	//Printing a whole array
 	//Suprise! There is no way to do that
 	//You can do that, if you iterate over it and save it into a string but u can do that yourself im pretty sure!
 	array_logger.Print("Array1:  +  Array-String!");
 
 	//Here is how...
 	std::string array_list;
-	for (int i = 0; i < array1.size(); i++)
+	for (const int& i : array1)
 	{
-		auto buf = std::to_string(array1.at(i));
+		auto buf = std::to_string(i);
 		array_list.append(buf + ", ");
 		buf.clear();
 	}
@@ -159,7 +161,8 @@ int main()
 
 	//Creating a new logger and vector
 	Cseoul_logger vector_logger(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
-	auto vector1 = std::vector<int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	auto vector1 = std::vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	vector1.reserve(20);
 
 	//Printing the vector at 0
 	vector_logger.Print(std::to_string(vector1.at(0)));
@@ -169,8 +172,7 @@ int main()
 	//See Line 151, it's the same!
 
 
-
-
+	//Cseoul_logger new_test_logger(Cseoul_logger::LOG_MODE::LOG_CONSOLE, Cseoul_logger::Cseoul_logger::LOG_WARNING);
 
 
 	return 0;
