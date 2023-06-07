946f21470c98bc1ce498acba3352e886482f88f8
Nick <91141907+seoulxss@users.noreply.github.com>
2023-06-07 19:03:44 +0200

    Update Logger.cpp
    Fixed a critical bug that caused a crash

--- Logger.cpp
+++ Logger.cpp
@@ -1,460 +1,188 @@
 #include "Logger.h"
+#include <array>
 
-bool priv_logger::helper::ShowMessageBox(const char* Text, const char* Title, bool beep)
+int main()
 {
-	if (beep == true)
+	//											//
+	//											//
+	//				Basic Functions				//
+	//											//
+	//											//
+
+
+	//How to create the object
+	//Gets init with console only & Successful level
+	//Both are the same!
+	//You can also create a object without these arguments
+	Cseoul_logger L1(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
+	Cseoul_logger L1_1(Cseoul_logger::LOG_MODE::LOG_CONSOLE, Cseoul_logger::LOG_LEVEL::LOG_SUCCESSFUL);
+	Cseoul_logger L1_2; //Will be initialized with Log_Console and Log_Error
+
+	//Printing with "Success" before the text and then also printing "Success!"
+	L1.Print("Success!");
+
+	//Set to false to hide the cur time
+	L1.set_time_use(false);
+
+	//True to show cur time
+	L1.set_time_use(true);
+
+	//set the log level
+	L1.set_log_level(Cseoul_logger::LOG_WARNING);
+
+	//Will print "WARNING" before the text
+	L1.Print("Warning!");
+
+	//This will create a Text file in the current folder    -> dont forget the .exe or whatever when u want to create it there
+	//If u have a dll to inject somewhere, it will be created in the applications exe folder
+	L1.Create_Log_File("Test_log_1", true, "", "Cseoul_logger.exe");
+
+	//This will create a Text file in a specific location
+	//As you can see, you can leave the "name_of_application" var empty, it will not affect anything
+	//*WARNING* Don't ever put 0 or NULL or nullptr in these empty vars, it will crash!
+	//Create only one file per logger!
+	L1.Create_Log_File("Test_log_2", false, "C:\\Users\\teilo\\Desktop\\Test", "");
+
+	//Here's the other way:
+	/* L1.Create_Log_File("Test_log_2", true, "", "Soul_logger.exe"); */
+
+
+	//Dont forget to set the log_mode after creating the file!
+	//U still have to create a file if u init the Logger with LOG_FILE_AND_CONSOLE
+	L1.set_log_mode(Cseoul_logger::LOG_FILE_AND_CONSOLE);
+
+
+	//U can use this to shutdown the logger (Destroying)
+	//But I woudln't recommend it unless you know what you're doing
+	//The destructor will handle everything automatically
+	//*WARNING*: Only Shutdown at the end of your program!
+	//**************** READ LINE 120 ********************
+	L1.Shutdown_Logger();
+
+	//														//
+	//														//
+	//				More "Advanced" functions				//
+	//														//
+	//														//
+
+	//Creating new logger
+	Cseoul_logger L2(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
+
+
+	//Sometimes you need to print values that arent strings, you can do that so:
+	L2.Print("Example int: " + std::to_string(2));
+
+	//This will not work -> acts like an array!
+	L2.Print("F1: " + 2);
+
+	//This will also not work -> acts like an array!
+	L2.Print("F2: " + 0x222);
+
+	//This will also not work -> acts like an array!
+	int some_addr = 0x2323;
+	L2.Print("F3: " + some_addr);
+
+	//Sometimes you have a address which u want to print and it prints the dec version
+	const int addr = 0x5555;
+	L2.Print("Addr: " + std::to_string(addr));
+
+	//You can fix this by using:
+	//This will print the hex value of your variable
+	L2.Print("Addr-hex: " + Logger::helper::value_to_string_hex<int>(addr));
+
+	//you can do that also with dec numbers:
+	L2.Print("Addr-dec: " + Logger::helper::value_to_string_dec(0x5555));
+
+
+
+	//														//
+	//														//
+	//				Multiple logger objects					//
+	//														//
+	//														//
+
+
+	//If u want to use multiple logger, init them all at ones!
+	//					Example					//
+
+	Cseoul_logger First_Logger(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
+	Cseoul_logger Second_Logger(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
+
+
+	//this is how you should NOT do it:
+	Cseoul_logger First_Logger_(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
+	// code....
+	// code....
+	Cseoul_logger Second_Logger_(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
+	// code...
+
+
+	//It is important, that the function Shutdown gets called at the end of the program.
+	//The destructor destroys everything at the end, so maybe it is better you never use the shutdown function.
+
+	//					Example					//
+	L1.Shutdown_Logger();
+	L2.Shutdown_Logger();
+	//Everything that gets printed here will be ignored (Even crash)
+	//For example:
+	//std::cout << "This will not be printed! AND Maybe even crash!" << "\n";
+	//return 0
+
+
+	//														//
+	//														//
+	//					Arrays, vectors...					//
+	//														//
+	//														//
+
+
+	//Creating a new logger and a array
+	Cseoul_logger array_logger(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
+	auto array1 = std::array<int, 10>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	//Printing the array at pos 0
+	array_logger.Print("array1: " + std::to_string(array1.at(0)));
+	array_logger.Print("array1: " + std::to_string(array1[0]));
+
+	//Printing a whole array
+	//Suprise! There is no way to do that
+	//You can do that, if you iterate over it and save it into a string but u can do that yourself im pretty sure!
+	array_logger.Print("Array1:  +  Array-String!");
+
+	//Here is how...
+	std::string array_list;
+	for (const int& i : array1)
 	{
-		MessageBeep(MB_ICONERROR);
-		MessageBoxA(nullptr, Text, Title, MB_ICONERROR);
-		return true;
+		auto buf = std::to_string(i);
+		array_list.append(buf + ", ");
+		buf.clear();
 	}
+	array_logger.Print(array_list);
 
-	else
-	{
-		MessageBoxA(nullptr, Text, Title, MB_ICONERROR);
-		return true;
-	}
-}
+	//Creating a new logger and vector
+	Cseoul_logger vector_logger(Cseoul_logger::LOG_CONSOLE, Cseoul_logger::LOG_SUCCESSFUL);
+	auto vector1 = std::vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+	//Printing the vector at 0
+	vector_logger.Print(std::to_string(vector1.at(0)));
 
 
-inline Cseoul_logger::LOG_LEVEL Cseoul_logger::get_log_level() const
-{
-	return Cseoul_logger::log_level;
-}
+	//Iteratung over a array:
+	//See Line 151, it's the same!
 
-Cseoul_logger::LOG_LEVEL Cseoul_logger::set_log_level(LOG_LEVEL new_log_level)
-{
-	return Cseoul_logger::log_level = new_log_level;
-}
+	//Cseoul_logger new_test_logger(Cseoul_logger::LOG_MODE::LOG_CONSOLE, Cseoul_logger::Cseoul_logger::LOG_WARNING);
 
-inline Cseoul_logger::LOG_MODE Cseoul_logger::get_log_mode() const
-{
-	return Cseoul_logger::log_mode;
-}
 
-Cseoul_logger::LOG_MODE Cseoul_logger::set_log_mode(LOG_MODE new_log_mode)
-{
-	return Cseoul_logger::log_mode = new_log_mode;
-}
 
-std::string Cseoul_logger::get_current_time()
-{
-	std::chrono::time_point<std::chrono::system_clock> t = std::chrono::system_clock::now();
-    time_t t_time_t = std::chrono::system_clock::to_time_t(t);
+	//														//
+	//														//
+	//				Color changing section					//
+	//														//
+	//														//
 
-	const std::tm* local_time = std::localtime(&t_time_t);
-    std::ostringstream oss;
+	Cseoul_logger color_logger;
 
-	 oss << std::put_time(local_time, "%H:%M:%S");
 
-	return oss.str();
-}
 
-bool Cseoul_logger::set_time_use(const bool val)
-{
-	Cseoul_logger::use_time = val;
-	return Cseoul_logger::use_time;
-}
-
-bool Cseoul_logger::get_time_use() const
-{
-	return Cseoul_logger::use_time;
-}
-
-HANDLE Cseoul_logger::get_console_handle() const
-{
-	return Cseoul_logger::console_handle;
-}
-
-HANDLE Cseoul_logger::set_console_handle()
-{
-	return Cseoul_logger::console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-}
-
-//Make sure to call Create_log_file when u want to log into a file!
-//This also gehts called automatically, so no need to call it again.
-bool Cseoul_logger::Init_Logger()
-{
-    if (Cseoul_logger::init_log == false)
-        Cseoul_logger::init_log = true;
-
-    if (Cseoul_logger::get_log_mode() == LOG_CONSOLE || Cseoul_logger::get_log_mode() == LOG_FILE_AND_CONSOLE)
-    {
-
-		if (Cseoul_logger::get_console_handle() != nullptr)
-			Console_already_There = true;
-
-		if (Cseoul_logger::get_console_handle() == nullptr)
-		{
-			AllocConsole();
-			freopen_s(&file_p, "CONOUT$", "w", stdout);
-
-		}
-
-
-    	if (Cseoul_logger::get_console_handle() == NULL)
-			Cseoul_logger::set_console_handle();
-
-
-        switch (Cseoul_logger::get_log_level())
-        {
-            case LOG_GENERAL:
-            {
-                std::cout << "Initialized Logger with: LOG_GENERAL level" << priv_logger::LOG_MACRO__::ENDL;
-                break;
-            }
-
-			case LOG_ERROR:
-			{
-				std::cout << "Initialized Logger with: ";
-				SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_RED);
-				std::cout << "LOG_ERROR";
-				SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_WHITE);
-				std::cout << " level" << priv_logger::LOG_MACRO__::ENDL;
-				break;
-			}
-
-            case LOG_SUCCESSFUL:
-            {
-				std::cout << "Initialized Logger with: ";
-				SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_GREEN);
-				std::cout << "LOG_SUCCESSFUL";
-				SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_WHITE);
-				std::cout << " level" << priv_logger::LOG_MACRO__::ENDL;
-				break;
-            }
-
-            case LOG_WARNING:
-            {
-				std::cout << "Initialized Logger with: ";
-				SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_YELLOW);
-				std::cout << "LOG_WARNING";
-				SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_WHITE);
-				std::cout << " level" << priv_logger::LOG_MACRO__::ENDL;
-				break;
-            }
-
-            default:
-            {
-                std::cout << "Initialized Logger with: Unknown level" << priv_logger::LOG_MACRO__::ENDL;
-                break;
-            }
-        }
-    }
-
-    return true;
-}
-
-bool Cseoul_logger::close_console_handle() const
-{
-	if (get_console_handle() == NULL || get_console_handle() == INVALID_HANDLE_VALUE)
-		return false;
-
-	else if (get_console_handle() != NULL && Console_already_There == true)
-	{
-		 CloseHandle(get_console_handle());
-		 return true;
-	}
-
-	return false;
-}
-
-//Make sure to call this, at the end of your program
-bool Cseoul_logger::Shutdown_Logger()
-{
-	if (CCounter::Oustanding_Objects() == 1)
-		FreeConsole();
-
-	if (file_p)
-		fclose(file_p);
-
-	file_p = nullptr;
-
-	if (get_console_handle() != nullptr && get_console_handle() != INVALID_HANDLE_VALUE && CCounter::Oustanding_Objects() == 1)
-		close_console_handle();
-
-	if (log_file.is_open())
-		log_file.close();
-
-	already_down = true;
-
-
-	return true;
-}
-
-//Prints something in the console
-void Cseoul_logger::Print(const std::string &text)
-{
-	if (Cseoul_logger::get_log_mode() == Cseoul_logger::LOG_FILE_AND_CONSOLE)
-	{
-		//Check if file is open etc..
-
-
-		switch (Cseoul_logger::get_log_level())
-		{
-		[[unlikely]] case Cseoul_logger::LOG_GENERAL:
-		{
-			if (Cseoul_logger::get_time_use() == true)
-			{
-				std::cout << "{" << Cseoul_logger::get_current_time() << "} ";
-			}
-			std::cout << "[";
-			SetConsoleTextAttribute(get_console_handle(), 9);
-			std::cout << "LOG_GENERAL";
-			SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_WHITE);
-			std::cout << "]" << " >> " << text << priv_logger::LOG_MACRO__::ENDL;
-			const auto temp_time = get_current_time();
-			log_file << "{" + temp_time + "}" + "[" + "LOG_GENERAL" + "]" + " >> " + text << priv_logger::LOG_MACRO__::ENDL;
-			break;
-
-			//Clsoe the file again
-		}
-
-		[[likely]] case Cseoul_logger::LOG_ERROR:
-		{
-			if (Cseoul_logger::get_time_use() == true)
-			{
-				std::cout << "{" << Cseoul_logger::get_current_time() << "} ";
-			}
-			std::cout << "[";
-			SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_RED);
-			std::cout << "LOG_ERROR";
-			SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_WHITE);
-			std::cout << "]" << " >> " << text << priv_logger::LOG_MACRO__::ENDL;
-			const auto temp_time = get_current_time();
-			log_file << "{" + temp_time + "}" + "[" + "LOG_ERROR" + "]" + " >> " + text << priv_logger::LOG_MACRO__::ENDL;
-			break;
-		}
-
-		[[likely]] case LOG_SUCCESSFUL:
-		{
-			if (Cseoul_logger::get_time_use() == true)
-			{
-				std::cout << "{" << Cseoul_logger::get_current_time() << "} ";
-			}
-			std::cout << "[";
-			SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_GREEN);
-			std::cout << "LOG_SUCCESSFUL";
-			SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_WHITE);
-			std::cout << "]" << " >> " << text << priv_logger::LOG_MACRO__::ENDL;
-			const auto temp_time = get_current_time();
-			log_file << "{" + temp_time + "}" + "[" + "LOG_SUCCESSFUL" + "]" + " >> " + text << priv_logger::LOG_MACRO__::ENDL;
-			break;
-		}
-
-		case LOG_WARNING:
-		{
-			if (Cseoul_logger::get_time_use() == true)
-			{
-				std::cout << "{" << Cseoul_logger::get_current_time() << "} ";
-			}
-			std::cout << "[";
-			SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_YELLOW);
-			std::cout << "LOG_WARNING";
-			SetConsoleTextAttribute(get_console_handle(), priv_logger::LOG_COLOR__::COLOR_WHITE);
-			std::cout << "]" << " >> " << text << priv_logger::LOG_MACRO__::ENDL;
-			const auto temp_time = get_current_time();
-			log_file << "{" + temp_time + "}" + "[" + "LOG_WARNING" + "]" + " >> " + text << priv_logger::LOG_MACRO__::ENDL;
-			break;
-		}
-
-		default:
-			break;
-
-		}
-
-	}
-
-	else if (Cseoul_logger::get_log_mode() == LOG_CONSOLE)
-	{
-		[[likely]]
-		switch (Cseoul_logger::get_log_level())
-		{
-		[[unlikely]] case LOG_GENERAL:
-		{
-			if (Cseoul_logger::get_time_use() == true)
-			{
-				std::cout << "{" << Cseoul_logger::get_current_time() << "} ";
-			}
-			std::cout << "[";
-			SetConsoleTextAttribute(get_console_handle(), 9);
-			std::cout << "LOG_GENERAL";
-			SetConsoleTextAttribute(get_console_handle(), 15);
-			std::cout << "]" << " >> " << text << priv_logger::LOG_MACRO__::ENDL;
-			break;
-		}
-
-		[[likely]] case LOG_SUCCESSFUL:
-		{
-			if (Cseoul_logger::get_time_use() == true)
-			{
-				std::cout << "{" << Cseoul_logger::get_current_time() << "} ";
-			}
-			std::cout << "[";
-			SetConsoleTextAttribute(get_console_handle(), 10);
-			std::cout << "LOG_SUCCESSFUL";
-			SetConsoleTextAttribute(get_console_handle(), 15);
-			std::cout << "]" << " >> " << text << priv_logger::LOG_MACRO__::ENDL;
-			break;
-		}
-
-		case LOG_WARNING:
-		{
-			if (Cseoul_logger::get_time_use() == true)
-			{
-				std::cout << "{" << Cseoul_logger::get_current_time() << "} ";
-			}
-
-			std::cout << "[";
-			SetConsoleTextAttribute(get_console_handle(), 14);
-			std::cout << "LOG_WARNING";
-			SetConsoleTextAttribute(get_console_handle(), 15);
-			std::cout << "]" << " >> " << text << priv_logger::LOG_MACRO__::ENDL;
-			break;
-		}
-
-		[[likely]] case LOG_ERROR:
-		{
-			if (Cseoul_logger::get_time_use() == true)
-			{
-				std::cout << "{" << Cseoul_logger::get_current_time() << "} ";
-			}
-
-			std::cout << "[";
-			SetConsoleTextAttribute(get_console_handle(), 12);
-			std::cout << "LOG_ERROR";
-			SetConsoleTextAttribute(get_console_handle(), 15);
-			std::cout << "]" << " >> " << text << priv_logger::LOG_MACRO__::ENDL;
-			break;
-		}
-
-		default:
-			break;
-		
-
-		}
-
-	}
-
-	else if (Cseoul_logger::get_log_mode() == LOG_FILE)
-{
-    switch (get_log_level())
-    {
-       [[unlikely]]case LOG_GENERAL:
-        {
-            if (log_file.is_open())
-            {
-                const auto temp_time = get_current_time();
-                log_file << "{" + temp_time + "}" + "[" + "LOG_GENERAL" + "]" + " >> " + text << priv_logger::LOG_MACRO__::ENDL;
-                break;
-            }
-            break;
-        }
-        [[likely]]case LOG_ERROR:
-        {
-            if (log_file.is_open())
-            {
-               const  auto temp_time = get_current_time();
-                log_file << "{" + temp_time + "}" + "[" + "LOG_ERROR" + "]" + " >> " + text << priv_logger::LOG_MACRO__::ENDL;
-                break;
-            }
-            break;
-        }
-        [[likely]]case LOG_SUCCESSFUL:
-        {
-            if (log_file.is_open())
-            {
-                const auto temp_time = get_current_time();
-                log_file << "{" + temp_time + "}" + "[" + "LOG_SUCCESSFUL" + "]" + " >> " + text << priv_logger::LOG_MACRO__::ENDL;
-                break;
-            }
-            break;
-        }
-        case LOG_WARNING:
-        {
-            if (log_file.is_open())
-            {
-                const auto temp_time = get_current_time();
-                log_file << "{" + temp_time + "}" + "[" + "LOG_WARNING" + "]" + " >> "+ text << priv_logger::LOG_MACRO__::ENDL;
-                break;
-            }
-            break;
-        }
-    }
-}
-
-	else
-	{
-		MessageBeep(MB_ICONERROR);
-		MessageBoxA(nullptr, "Weird!", "test!", 0);
-		return;
-	}
-
-}
-
-std::string Cseoul_logger::get_exe_file_name()
-{
-	char buff[MAX_PATH];
-	GetModuleFileNameA(NULL, buff, MAX_PATH);
-	return std::string(buff);
-}
-
-bool Cseoul_logger::Create_Log_File(const std::string log_file_name, bool in_directory, std::string Location, std::string name_of_application)
-{
-
-	if (in_directory == true && log_file_name.empty() != true)
-	{
-		std::string log = get_exe_file_name();
-
-		const auto string_check = log.find(name_of_application);
-
-		if (string_check != std::string::npos)
-		{
-			log.erase(string_check, name_of_application.length());
-		}
-
-		auto end_var = log;
-		end_var.append(log_file_name + ".txt");
-
-
-		if (std::filesystem::exists(end_var))
-			std::filesystem::remove(end_var);
-
-		log_file.open(end_var);
-		return true;
-	}
-
-
-	else if (in_directory == false && Location.empty() != true)
-	{
-
-		if (std::filesystem::exists(Location))
-		{
-			Location.append("\\");
-			Location.append(log_file_name + ".txt");
-
-			if (std::filesystem::exists(Location))
-				std::filesystem::remove(Location);
-
-			
-		
-			Cseoul_logger::log_file_location = Location;
-			Cseoul_logger::log_file.open(Location);
-			return true;
-			
-
-
-		}
-
-
-	}
-
-	else [[unlikely]]
-	{
-		priv_logger::helper::ShowMessageBox("Something went wrong", "Error Create_log_file!", true);
-		return false;
-	}
-
-	return false;
+	return 0;
 }
